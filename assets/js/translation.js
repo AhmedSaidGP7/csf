c4a8698b089c037fd1c384fcd07365646131e15c
HazemSaii <hazem.jaheen@saiiros.com>
2024-09-07 12:04:46 +0300

    translation

---
+++ assets/js/translation.js
@@ -0,0 +1,91 @@
+const translations = {
+    en: {
+        introduction: "INTRODUCING SOCIATHON",
+        compare:"COMPETE TO SHAPE THE FUTURE OF CIVIL SOCIETY",
+        int_1: `"Sociathon" is the newest annual initiative from the "Civil Society Support Fund" under the portage of the Ministry of Social Solidarity. This year's inaugural event aims to inspire designers and creatives nationwide to participate in a competitive process to craft a visual identity for the CSF. This unique competition will produce a logo that will be widely recognized by Egyptians and remembered for years to come.`,
+        int_2: `Every year, this initiative focuses on engaging the community in addressing the various challenges faced by civil society organizations. For this year, the spotlight is on a brand identity design contest that will represent the Civil Society Support Fund, showcasing its vision and mission.`,
+        int_3: `We invite designers, creatives, and innovative thinkers to join the competition, offering more than just an artistic creation. Your participation will contribute significantly to advancing development efforts in Egypt.`,
+        int_4: `We eagerly await your designs and how they will demonstrate our commitment to empowering and strengthening civil society. Make an impact and showcase your creativity today!`,
+        int_5: `CIVIL SOCIETY SUPPORT FUND.`,
+        int_6: `Established under Law No. 149 of 2019, the Civil Society Support Fund is an independent entity under the Ministry of Social Solidarity. The fund is dedicated to promoting sustainable development, social and climate justice, and meeting humanitarian needs while fostering positive citizenship values and encouraging volunteerism. It supports civil society organizations through financial, technical, and administrative means, adhering to principles of accountability, transparency, and sustainability.`,
+        int_7: `The fund's mission is to create a supportive environment for civil society that is aligned with key national priorities through well-planned initiatives in social services and economic support. It focuses on addressing critical issues across all sectors, adopting a results-driven management approach, and diversifying funding sources to strengthen all areas of civil society.`,
+        timeline_title: 'Competition Timeline',
+        timeline_1: 'Submission Deadline',
+        timeline_2: 'Shortlist Announcement',
+        timeline_3: 'Closed Workshop Begins',
+        timeline_4: 'Voting for Best Identity',
+        timeline_5: 'Winner Announcement',
+        timeline_6: 'Visual Identity Creation',
+        timeline_date: '31 AUG 2024',
+        participate_title: "Win and Make a Difference: Your Chance to Shine and Create Change",
+        participate_desc_1: "The prize will be equally divided between the winner and a charity project of their choice in Egypt.",
+        participate_desc_2: "The winner will select the form of charity in cooperation with a civil society organization of their choice.",
+        voting_title: "Voting for the Final Visual Identities of the Contestants",
+        voting_process: "The voting will take place in three stages:",
+        voting_step_1: "All final identities will be voted on by a selected group of creatives and specialists in graphic design.",
+        voting_step_2: "The top 10 identities will be chosen by public voting on our social media platform.",
+        voting_step_3: "The shortlisted identities will be presented for the final vote by the jury to choose the winner.",
+         prize_title: "The Prize Offered",
+         visual_identity_title: "Terms and Conditions",
+         visual_identity_body: "Download this file to know morea bout Sociathon, and the competition terms and conditions",
+         email_subject: "Make sure the email subject is your National ID or Passport number or other ID number for your submission to be accepted.",
+         extra_pdf: "You can attach an extra PDF file to visualize the identity you have worked on better.",
+         identity_concept:"Submit Your CSF Visual Identity Concept",
+         identity_desc:"Make sure the email subject is your National ID or Passport number or other ID number for your submission to be accepted.",
+         identity:"Make sure the email subject is your National ID or Passport number or other ID number for your submission to be accepted."
+    },
+    ar: {
+        introduction: "مقدمـــــــــــــــة <br> عن سوشياثون" ,
+        compare:"نتنافس لتشكيل مستقبل<br> المجتمــــــــــــع الأهلــــــــي",
+        int_1: `"سوشياثون" هي المبادرة السنوية الأحدث من "صندوق دعم المجتمع الأهلي،" تحت رعاية وزارة التضامن الاجتماعي! وتهدف في نسختها الأولى تشجيع المصممين والمبدعين من أنحاء الجمهورية للمنافسة  النزيهة حول تصميم هوية بصرية لهئية وطنية، وهي المسابقة الأولى من نوعها لتصميم شعار يراه كل مصري، و سيظل في الذاكرة زمناً طويلاً.`,
+        int_2: `في كل عام، سوف تركز المبادرة على إشراك المجتمع في إيجاد الحلول للتحدياتٍ المختلفة التي تواجه منظمات المجتمع المدني. في نسخة هذا العام، تتمركز المبادرة حول مسابقة تصميم هوية بصرية (Brand Identity) تمثّل صندوق دعم المجتمع الاهليّ بما يعكس رؤية ورسالة صندوق دعم المجتمع الأهلي.`,
+        int_3: `ندعو المصممين، والمبدعين، وذوي الأفكار الخلاقة إلى المشاركة في المسابقة، والمساهمة - لا في تقديم منتج فني فحسب- بل في تطوير واقع العمل التنموي في مصر.`,
+        int_4: `نتطلع إلى رؤية إبداعاتكم وكيف ستعبر عن التزامنا العميق لتمكين وتقوية المجتمع المدني. اصنعوا فارقاً، واتركوا لمستكم الابداعية الآن!`,
+        int_5: `صندوق دعم العمل الأهلي`,
+        int_6: `صندوق دعم المجتمع الأهلي، الذي أنشأ بموجب القانون رقم 149 لسنة 2019، هي هيئة وطنية مستقلة. يهدف الصندوق إلى تعزيز التنمية المستدامة، والعدالة الاجتماعية والمناخية، وتلبية الاحتياجات الإنسانية، مع تعزيز قيم المواطنة الإيجابية وتشجيع العمل التطوعي. يقدم الصندوق دعمه لمنظمات المجتمع المدني من خلال وسائل مالية وفنية وإدارية، مع الالتزام بمبادئ المساءلة والشفافية والاستدامة.`,
+        int_7: `تتمثل مهمة الصندوق في توفير بيئة داعمة للمجتمع المدني، تتوافق مع أبرز الأولويات الوطنية، من خلال عدة مبادرات مدروسة في مجالات الخدمة الاجتماعية والدعم الاقتصادي. يركز الصندوق على عدة قضايا ملحّة في كافة القطاعات. يتنبى صندوق دعم المجتمع الأهلي نهج إدارة قائم على النتائج الايجابية وتنويع مصادر التمويل لدعم كافة قطاعات المجتمع المدني.`,
+        timeline_title: 'المخطط الزمني للمسابقة',
+        timeline_1: 'آخر موعد للتسليم',
+        timeline_2: 'الإعلان عن القائمة القصيرة',
+        timeline_3: 'بدء ورشة العمل المغلقة',
+        timeline_4: 'التصويت على أفضل هوية',
+        timeline_5: 'الإعلان عن الفائز بالمسابقة',
+        timeline_6: 'إنشاء عناصر الهوية البصرية',
+        timeline_date: '31 AUG 2024',
+        participate_title: "اكسب وشارك في الخير: فرصتك للتألق وعمل تغيير",
+        participate_desc_1: "سيتم تقسيم الجائزة بالتساوي بين الفائز ومشروع خيري يختاره في جمهورية مصر العربية.",
+        participate_desc_2: "يختارها الفائز شكل العمل الخيري بالتعاون مع منظمة مجتمع أهلي من اختياره.",
+        voting_title: "التصويت على الهويات البصرية النهائية للمتسابقين",
+        voting_process: "يتم على ثلاثة مراحل:",
+        voting_step_1: "يتم التصويت على جميع الهويات النهائية عن طريق مجموعة مختارة من المبدعين والمتخصصين بمجال التصميم الجرافيكي.",
+        voting_step_2: "يتم التصويت علي أفضل 10 هويات عن طريق التصويت العلني على منصتنا على السوشيال ميديا.",
+        voting_step_3: "يتم عرض القائمة المتأهلة للتصويت النهائي من خلال لجنة التحكيم لاختيار الفائز.",
+        prize_title: "الجائزة المقدمة",
+        visual_identity_title: "قواعد وشروط المسابقة",
+        visual_identity_body: "قم بتحميل المستند التالي للتعرف على تفاصيل مبادرة“سوشياثون”و قواعد وشروط المشاركة.",
+        email_subject: "يمكنك إرفاق ملف إضافي بصيغة .pdf لإيضاح الهوية البصرية بشكل أفضل",
+        extra_pdf: "يمكنك إرفاق ملف إضافي بصيغة .pdf لإيضاح الهوية البصرية بشكل أفضل.",
+        identity_concept:"ارسل مخطط تصميم الهوية البصرية للصندوق",
+        identity_desc:"بعد التسجيل، لا تنس أن ترسل التصور المبدأي للهوية البصرية على البريد الإلكتروني التالي. (يجب أن تكون صيغة الملف .svg أو .ai)",
+        identity:"ضع عنوان البريد الإلكتروني  (سوشياثون +رقمك القومي/رقم جواز السفر/رقم وثيقة إقامة أو لجوء لغير المصريين)."
+
+    }
+};
+
+function changeLanguage(language) {
+    document.querySelectorAll('[data-translate]').forEach(element => {
+        const key = element.getAttribute('data-translate');
+        // Set innerHTML to preserve the structure of the HTML like <br> tags
+        element.innerHTML = translations[language][key];
+    });
+
+    // Set the language direction for Arabic
+   
+
+    localStorage.setItem('selectedLanguage', language);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLanguage = localStorage.getItem('selectedLanguage') || 'ar';
+    changeLanguage(savedLanguage);
+});
